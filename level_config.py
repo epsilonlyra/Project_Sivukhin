8ecbaa5b088babb9b033225fa2cccf6e6400e153
knyazevmatvey <knyazevmatvey@yandex.ru>
2021-12-13 19:54:41 +0300

    Added create_level function to level_config

--- level_config.py
+++ level_config.py
@@ -8,16 +8,40 @@ pygame.init()
 pygame.display.init()
 screen = pygame.display.set_mode((10, 10)) # это нужно чтобы работал convert
 
-def create_level(number, x, y, change_size = False, x_picture = None, y_picture = None,):
+def create_level(number, x_destr, y_destr, x_ind, y_ind, duck_info,
+                 change_size = False, x_picture = None, y_picture = None):
     num = str(number)
     if change_size:
-        get_obstacles(fetch_file('pictures', 'destr.png','levels', 'level'+num,
-                                 x_size = x_picture, y_size = y_picture), x, y)
+        def destr():
+            return get_obstacles(fetch_file('pictures', 'destr.png','levels', 'level'+num,
+                                 x_size = x_picture, y_size = y_picture), x_destr, y_destr)
+        def indestr():
+            return get_obstacles(fetch_file('pictures', 'indestr.png','levels', 'level'+num,
+                                 x_size = x_picture, y_size = y_picture), x_ind, y_ind)
     else:
-        destr = get_obstacles(fetch_file('pictures', 'destr.png', 'levels', 'level'+num), x, y)
-        indestr = get_obstacles(fetch_file('pictures', 'indestr.png','levels', 'level'+num), x, y)
-    duck_r
+        def destr():
+            return get_obstacles(fetch_file('pictures', 'destr.png', 'levels', 'level'+num), x_destr, y_destr)
+        def indestr():
+            return get_obstacles(fetch_file('pictures', 'indestr.png','levels', 'level'+num), x_ind, y_ind)
+
+    def duck_function():
+        duck_list = []    
+        for d in duck_info:
+            if len(d) == 2:
+                duck_x, duck_y = d
+                duck_list.append(Duck(None, 30, duck_x, duck_y, using_mask = True))
+            if len(d) == 3:
+                duck_x, duck_y, f = d
+                duck_list.append(Duck(None, 30, duck_x, duck_y, using_mask = True, faculty = f))
+        return duck_list
+
+    level = dict({'destr' : destr, 'indestr' : indestr, 'ducks' : duck_function})
+
+    return level
+
+level1 = create_level(1, 340, 240, 340, 440, [(400, 200)])
     
+'''  
 
 # level1 config
 
@@ -38,6 +62,7 @@ def level1_ducks():
 
 level1 = dict({'destr' : level1_destr, 'indestr' : level1_indestr,
               'ducks' : level1_ducks})
+'''
 
 # level2 config
 
