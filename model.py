98399e6ea9f4008852966d884f5fbd0e78c1450f
knyazevmatvey <knyazevmatvey@yandex.ru>
2021-12-15 20:46:16 +0300

    Refactored model

--- model.py
+++ model.py
@@ -1,24 +1,20 @@
 import math
 import numpy as np
-import pygame
 
-BLUE = 0x0000FF
-BLACK = 0x000000
-WHITE = 0xFFFFFF
-rho_0 = 10
-speed = 0.5
-h = 5
-g = 5
-r_ball = 6
-m = 1
 
-k = 100  # parameters for state equation
+# parameters for smoothing and particles
+h = 5   # smoothing depth
+m = 1   # particle mass
+g = 5   # free fall acceleration
+
+# parameters for state equation
+k = 100
 power = 35
 nu = 0.2
 dt = 0.2
 
 
-def kernel(x, y, h):
+def kernel(x, y):
     """
     Gausssian Smoothing kernel (2D)
     x, y - coordinates
@@ -30,11 +26,11 @@ def kernel(x, y, h):
     return w
 
 
-def grad_kernel(x, y, h):
+def grad_kernel(x, y):
     """
     Gradient of the Gausssian Smoothing kernel (2D)
-    x, y - вектора координат
-    h - глубина сглаживния
+    x, y - coordinates
+    h - smoothing depth
     """
 
     r = np.sqrt(x ** 2 + y ** 2)
@@ -47,12 +43,12 @@ def grad_kernel(x, y, h):
 
 def relative_coordinates(r):
     """
-    Дает относительные координаты j-й частицы относительно i-й
-    r - матрица nx2
+    Returns coordinates of the j-th particle relative to the i-th one
+    r - nx2 matrix
     """
-    M = r.shape[0]
-    rx = r[:, 0].reshape((M, 1))
-    ry = r[:, 1].reshape((M, 1))
+    size = r.shape[0]
+    rx = r[:, 0].reshape((size, 1))
+    ry = r[:, 1].reshape((size, 1))
 
     dx = rx - rx.T
     dy = ry - ry.T
@@ -60,40 +56,38 @@ def relative_coordinates(r):
     return dx, dy
 
 
-def density(r, m, h):
+def density(r):
     """
-    Возвращает плотность в местах нахождения частиц
-    r - матрица nx2 координат частиц
-    m - масса частиц
-    h - глубина сглаживания
+    Returns density matrix at particle positions
+    r - nx2 coordinate matrix
     """
     dx, dy = relative_coordinates(r)
-    M = r.shape[0]
-    rho = np.sum(m * kernel(dx, dy, h), 1).reshape((M, 1))
+    size = r.shape[0]
+    rho = np.sum(m * kernel(dx, dy), 1).reshape((size, 1))
 
     return rho
 
 
 def pressure(rho):
     """
-    Возвращает давление
+    Returns pressure
     """
     p = k * rho ** (1 + 1 / power)
 
     return p
 
 
-def acceleration(r, v, m, h, nu):
+def acceleration(r, v):
     """
-    Возвращает ускорение частиц
+    Returns acceleration
     """
-    rho = density(r, m, h)
+    rho = density(r)
     p = pressure(rho)
     dx, dy = relative_coordinates(r)
-    dWx, dWy = grad_kernel(dx, dy, h)
+    d_wx, d_wy = grad_kernel(dx, dy)
 
-    ax = -np.sum(m * (p / rho ** 2 + p.T / rho.T ** 2) * dWx, 1)
-    ay = -np.sum(m * (p / rho ** 2 + p.T / rho.T ** 2) * dWy, 1)
+    ax = -np.sum(m * (p / rho ** 2 + p.T / rho.T ** 2) * d_wx, 1)
+    ay = -np.sum(m * (p / rho ** 2 + p.T / rho.T ** 2) * d_wy, 1)
     ay += g
     a = np.array([ax, ay])
     a = a.T
@@ -104,20 +98,27 @@ def acceleration(r, v, m, h, nu):
 
 def reflect(vx, vy, alpha):
     """
-    vx, vy - координаты скорости
-    alpha - угол
+    Returns velocity components after the collision of the particle with a wall
+    vx, vy - velocity coordinates
+    alpha - angle from the center of the particle to the point of contact with the wall
     """
+
     u = vy * math.cos(alpha) - vx * math.sin(alpha)
     w = vx * math.cos(alpha) + vy * math.sin(alpha)
 
-    k = 1
-    ux = -u * math.sin(alpha) - k * w * math.cos(alpha)
-    uy = u * math.cos(alpha) - k * w * math.sin(alpha)
+    elasticity = 1
+    ux = -u * math.sin(alpha) - elasticity * w * math.cos(alpha)
+    uy = u * math.cos(alpha) - elasticity * w * math.sin(alpha)
 
     return ux, uy
 
 
 def make_water(left, right, up, down, number):
+    """
+    Creates water particles in given rectangle
+    left, right, up, down - corresponding boundaries
+    number - number of particles being created
+    """
     x = np.random.randint(left, (right + left) / 2, number).astype(np.float64)
     y = np.random.randint((up + down) / 2, down, number).astype(np.float64)
     r = np.array([x, y]).T
@@ -127,12 +128,12 @@ def make_water(left, right, up, down, number):
 
 
 def step(r, v):
-    acc = acceleration(r, v, m, h, nu)
+    """
+    Returns r and v at time t+dt given r and v at time t
+    """
+    acc = acceleration(r, v)
 
     v += acc * dt
     r += v * dt
 
     return r, v
-
-
-pygame.quit()
