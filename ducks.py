fa0b63249ec526d41dd8ffbaa4c06d732c0ccd07
knyazevmatvey <knyazevmatvey@yandex.ru>
2021-12-15 20:21:05 +0300

    Refactored ducks by removing no longer needed stuff

--- ducks.py
+++ ducks.py
@@ -9,47 +9,36 @@ import buttons
 class Duck:
     duck_array = []
 
-    def __init__(self, function, max_drop, x=-1000, y=-1000, using_mask=False, faculty='fpmi'):
+    def __init__(self, max_drop, x=-1000, y=-1000, faculty='fpmi'):
         """
-        function - функция, которая принимает координаты воды и возвращает:
-        1, если частица попала в утку
-        0, если частица не попала в утку
-        (Т. е. function задает форму утки)
-        max_drop - количество капель воды, которое должна получить утка, чтобы исчезнуть
-        x, y - координаты утки
-        using_mask - в случае True вместо function просто проверяется столкновение с маской
-        faculty - определяет вид утки
+        max_drop - max number of water particles consumed by the duck
+        x, y - duck coordinates
         """
 
         self.water = 0
         self.level = 0
-        self.func = function
         self.max = max_drop
         self.x = x
         self.y = y
         self.surf = duck_image[faculty][0]
         self.mask = pg.mask.from_surface(self.surf)
-        self.using_mask = using_mask
         self.faculty = faculty
 
     def check(self, x, y, drop_mask):
         """
-        x, y - координаты частицы
-        Возвращает True, если частица попала в утку, False - иначе
+        x, y - duck coordinates
+        Returns True if the particle hits the duck, False otherwise
         """
-        if self.using_mask:
-            x = int(x)
-            y = int(y)
-            offset = int(self.x) - x, int(self.y) - y
-            crisis = drop_mask.overlap(self.mask, offset)
-            return crisis
-        else:
-            return self.func(x, y)
+        x = int(x)
+        y = int(y)
+        offset = int(self.x) - x, int(self.y) - y
+        crisis = drop_mask.overlap(self.mask, offset)
+        return crisis
 
     def upgrade(self):
         """
-        Меняет level соответственно поглощенной воде water
-        level - характеристика заполненности утки водой
+        Changes level based on consumed water
+        level - determines duck's stage in life
         """
         if self.water >= self.max:
             self.level = 3
@@ -61,49 +50,33 @@ class Duck:
         self.mask = self.mask = pg.mask.from_surface(self.surf)
 
 
-def circle_function(x_duck, y_duck, r):
-    """
-    Возвращает функцию, которая возвращает 1, если x, y находятся внутри круга радиуса r
-    с координатами x_duck, y_duck, и возвращает 0 иначе
-    (Использовалась для создания круглой утки)
-    """
-
-    def f(x, y):
-        if (x - x_duck) ** 2 + (y - y_duck) ** 2 <= r ** 2:
-            return True
-        else:
-            return False
-
-    return f
-
-
 def record_destroying_duck(faculty):
     """
-    Записывает в json файл факт спития утки вида faculty
+    Records in json file the fact of duck being filled with water
     """
-    with open('record.json') as f:
-        data = json.load(f)
+    with open('record.json') as file:
+        data = json.load(file)
     if len(data) == 0:
         data = [faculty]
     else:
         if faculty not in data:
             data.append(faculty)
-    with open('record.json', 'w') as f:
-        json.dump(data, f)
+    with open('record.json', 'w') as file:
+        json.dump(data, file)
 
 
 def get_faculties():
     """
-    Возвращает массив из json
+    Returns array of ducks from json
     """
-    with open('record.json') as f:
-        data = json.load(f)
-    with open('record.json', 'w') as f:
-        json.dump(data, f)
+    with open('record.json') as file:
+        data = json.load(file)
+    with open('record.json', 'w') as file:
+        json.dump(data, file)
     return data
 
 
-# создание картинок для уток
+# creating images for the ducks
 mipt = ['dgap', 'fpmi', 'faki', 'falt', 'frtk', 'fupm', 'fpfe']
 duck_image = {}
 for f in mipt:
@@ -112,5 +85,3 @@ for f in mipt:
         duck_pick = buttons.Picture(0, 0, buttons.fetch_file(os.path.join('pictures', 'ducks'), f + '.png'),
                                     40 + 20 * i, 40 + 20 * i)
         duck_image[f].append(duck_pick.image)
-
-        
--- level_config.py
+++ level_config.py
@@ -48,12 +48,12 @@ def create_level(number, x_destr, y_destr, x_ind, y_ind, duck_info,
         for d in duck_info:
             if len(d) == 2:
                 duck_x, duck_y = d
-                duck_list.append(ducks.Duck(None, 30,
-                                            duck_x, duck_y, using_mask=True))
+                duck_list.append(ducks.Duck(30,
+                                            duck_x, duck_y))
             if len(d) == 3:
                 duck_x, duck_y, f = d
-                duck_list.append(ducks.Duck(None, 30, duck_x,
-                                            duck_y, using_mask=True, faculty=f))
+                duck_list.append(ducks.Duck(30, duck_x,
+                                            duck_y, faculty=f))
         return duck_list
 
     level = dict({'destr': destr, 'indestr': indestr, 'ducks': duck_function})
